172a550629d6e0428bb687f33a8b255163c24f9a
sasidhar sunny <63310620+sasidharsunny@users.noreply.github.com>
2023-12-14 10:46:02 +0530

    Update install.sh

--- envs/dev/modules/ec2/install.sh
+++ envs/dev/modules/ec2/install.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 #VM Startup Script
+# sudo yum update -y
+# sudo amazon-linux-extras install docker -y
+# sudo systemctl enable docker --now
+# sudo systemctl enable containerd.service --now
+# sudo usermod -a -G docker ec2-user
+# sudo docker pull bjwrd/app.py
+# sudo docker run -d bjwrd/app.py 
+
+or 
+
+#!/bin/bash
+#VM Startup Script
+#!/bin/bash
+
+# Update the package repository and install a web server (e.g., Apache)
 sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo systemctl enable docker --now
-sudo systemctl enable containerd.service --now
-sudo usermod -a -G docker ec2-user
-sudo docker pull bjwrd/app.py
-sudo docker run -d bjwrd/app.py 
+sudo yum install -y httpd
+
+# Start the web server
+sudo service httpd start
+
+# Create a directory for the web content
+sudo mkdir /var/www/html
+
+# Create and deploy the index.html file
+echo "<html><head><title>My Website</title></head><body><h1>Hello, AWS!</h1></body></html>" | sudo tee /var/www/html/index.html
+
+# Set the correct permissions
+sudo chmod 644 /var/www/html/index.html
+
+# Restart the web server to apply changes
+sudo service httpd restart
+
